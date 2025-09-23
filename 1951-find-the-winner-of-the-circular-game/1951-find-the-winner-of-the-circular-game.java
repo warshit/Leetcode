4c192c302262c62b630f7ba5cae4ef3d34e29828
warshit <155526453+warshit@users.noreply.github.com>
2025-09-23 10:29:24 +0530

    Time: 0 ms (100%), Space: 40.6 MB (76.44%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,15 +1,20 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=1;i<=n;i++){
-            q.offer(i);
+        // Queue<Integer> q = new LinkedList<>();
+        // for(int i=1;i<=n;i++){
+        //     q.offer(i);
+        // }
+        // while(q.size()>1){
+        //     for(int i=0;i<k-1;i++){
+        //         q.offer(q.poll());
+        //     }
+        //     q.poll();
+        // }
+        // return q.poll();
+        int winner = 0;
+        for(int i=2;i<=n;i++) {
+            winner = (winner+k) % i;
         }
-        while(q.size()>1){
-            for(int i=0;i<k-1;i++){
-                q.offer(q.poll());
-            }
-            q.poll();
-        }
-        return q.poll();
+        return winner+1;
     }
 }
