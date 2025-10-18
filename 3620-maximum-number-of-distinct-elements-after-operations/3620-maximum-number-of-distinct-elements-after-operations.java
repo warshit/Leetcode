430a32e36ca46495bc98f7d24675aab8e0fd8c66
warshit <155526453+warshit@users.noreply.github.com>
2025-10-18 23:20:01 +0530

    Time: 10 ms (97.24%), Space: 58 MB (81.38%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.java
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.java
@@ -1,31 +1,16 @@
 class Solution {
     public int maxDistinctElements(int[] nums, int k) {
-          int n = nums.length;
-        long[][] intervals = new long[n][2];
-
-        for (int i = 0; i < n; i++) {
-            intervals[i][0] = (long) nums[i] - k; // start
-            intervals[i][1] = (long) nums[i] + k; // end
-        }
-
-        Arrays.sort(intervals, (a, b) -> Long.compare(a[0], b[0]));
-
-        long current = Long.MIN_VALUE;
-        int count = 0;
-
-        for (long[] range : intervals) {
-            long left = range[0];
-            long right = range[1];
-
-            if (current < left) {
-                current = left;
-                count++;
-            } else if (current + 1 <= right) {
-                current++;
-                count++;
+          if (nums.length <= (k << 1) + 1) return nums.length;
+        Arrays.sort(nums);
+        int distinct = 0;
+        int l = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int m = Math.max(l+1, nums[i]-k);
+            if (m <= nums[i]+k) {
+                distinct++;
+                l = m;
             }
         }
-
-        return count;
+        return distinct;
     }
 }
