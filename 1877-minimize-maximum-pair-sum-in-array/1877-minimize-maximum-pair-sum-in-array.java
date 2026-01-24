3c3943e757a2e1e0215a063d57e52b4a1ee1990e
warshit <155526453+warshit@users.noreply.github.com>
2026-01-24 23:38:57 +0530

    Time: 6 ms (99.34%), Space: 98 MB (5.16%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -1,18 +1,47 @@
 class Solution {
     public int minPairSum(int[] nums) {
-         Arrays.sort(nums);
+        //  Arrays.sort(nums);
         
-        int left = 0;
-        int right = nums.length - 1;
-        int maxSum = 0;
+        // int left = 0;
+        // int right = nums.length - 1;
+        // int maxSum = 0;
         
-        while (left < right) {
-            int pairSum = nums[left] + nums[right];
-            maxSum = Math.max(maxSum, pairSum);
-            left++;
-            right--;
+        // while (left < right) {
+        //     int pairSum = nums[left] + nums[right];
+        //     maxSum = Math.max(maxSum, pairSum);
+        //     left++;
+        //     right--;
+        // }
+        
+        // return maxSum;
+         int minMax = 0;
+
+        for(int n:nums){
+            minMax = Math.max(n,minMax);
         }
-        
-        return maxSum;
+
+        int[] arr = new int[minMax+1];
+        for(int n:nums){
+            arr[n]++;
+        }
+
+        int l = 0, r = minMax, ans = 0;
+        while (l < r) {
+            while (arr[l] == 0)
+                l++;
+            while (arr[r] == 0)
+                r--;
+            ans = Math.max(ans, l + r);
+            if (arr[r] > arr[l])
+                arr[r] -= arr[l++];
+            else if (arr[l] > arr[r])
+                arr[l] -= arr[r--];
+            else {
+                l++;
+                r--;
+            }
+        }
+
+        return ans;
     }
 }
