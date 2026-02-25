b8b8bbdde0238f0c6e41a82123e1841cbc8ae3cf
warshit <155526453+warshit@users.noreply.github.com>
2026-02-25 21:57:44 +0530

    Time: 2 ms (100%), Space: 46.5 MB (84.05%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,24 +1,58 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-         Integer[] b = new Integer[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            b[i] = arr[i];
-        }
+        //  Integer[] b = new Integer[arr.length];
+        // for (int i = 0; i < arr.length; i++) {
+        //     b[i] = arr[i];
+        // }
         
-        Arrays.sort(b, (x, y) -> {
-            int c = Integer.bitCount(x);
-            int d = Integer.bitCount(y);
+        // Arrays.sort(b, (x, y) -> {
+        //     int c = Integer.bitCount(x);
+        //     int d = Integer.bitCount(y);
             
-            if (c == d) {
-                return x - y;
-            }
-            return c - d;
-        });
+        //     if (c == d) {
+        //         return x - y;
+        //     }
+        //     return c - d;
+        // });
         
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = b[i];
-        }
+        // for (int i = 0; i < arr.length; i++) {
+        //     arr[i] = b[i];
+        // }
         
+        // return arr;
+        int n = arr.length;
+        for(int i = 0; i < n; i++) arr[i] += 10001 * Integer.bitCount(arr[i]);
+        quicksort(arr, 0, n - 1);
+        for(int i = 0; i < n; i++) arr[i] %= 10001;
         return arr;
+    }
+    private static void quicksort(int[] nums, int left, int right) {
+        if(left < right) {
+            int part = partition(nums, left - 1, right + 1);
+            quicksort(nums, left, part);
+            quicksort(nums, part + 1, right);
+        }
+    }
+    private static int partition(int[] nums, int left, int right) {
+        int current = getPivot(nums[left + 1], nums[left + right >>> 1], nums[right - 1]), temp = 0;
+        while(true) {
+            do {
+                left++;
+            }while(nums[left] < current);
+            do {
+                right--;
+            }while(nums[right] > current);
+            
+            if(left >= right) return right;
+
+            temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+        }
+    }
+    private static int getPivot(int a, int b, int c) {
+        if((a >= b) ^ (a >= c)) return a;
+        if((a >= b) ^ (c >= b)) return b;
+        return c;
     }
 }
