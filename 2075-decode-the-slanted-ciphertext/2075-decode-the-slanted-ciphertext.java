347f768f1a137ceb59c125edb012acd5d5b133ce
warshit <155526453+warshit@users.noreply.github.com>
2026-04-04 19:30:31 +0530

    Time: 10 ms (99.41%), Space: 57.3 MB (91.12%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
@@ -1,33 +1,78 @@
 class Solution {
-    public String decodeCiphertext(String s, int r) {
-        if (r == 0) return "";
+//     public String decodeCiphertext(String s, int r) {
+//         if (r == 0) return "";
 
-        int n = s.length();
-        int c = n / r;
+//         int n = s.length();
+//         int c = n / r;
 
-        char[][] m = new char[r][c];
-        int k = 0;
+//         char[][] m = new char[r][c];
+//         int k = 0;
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                m[i][j] = s.charAt(k++);
+//         for (int i = 0; i < r; i++) {
+//             for (int j = 0; j < c; j++) {
+//                 m[i][j] = s.charAt(k++);
+//             }
+//         }
+
+//         StringBuilder sb = new StringBuilder();
+
+//         for (int j = 0; j < c; j++) {
+//             int i = 0, x = j;
+//             while (i < r && x < c) {
+//                 sb.append(m[i][x]);
+//                 i++;
+//                 x++;
+//             }
+//         }
+
+//         int e = sb.length() - 1;
+//         while (e >= 0 && sb.charAt(e) == ' ') e--;
+
+//         return sb.substring(0, e + 1);
+//     }
+// }
+   public String decodeCiphertext(String encodedText, int rows) {
+        if (rows == 1) return encodedText;
+        int encodedSize = encodedText.length();
+        int columns = encodedSize/rows;
+        char[][] matrix = new char[rows][columns];
+
+        int c = 0;
+        for (int i = 0; i < rows; i++) {
+            if (c == encodedSize) break;
+            for (int j = 0; j < columns; j++) {
+                if (c == encodedSize) break;
+                matrix[i][j] = encodedText.charAt(c);
+                c++;
             }
         }
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder originalText = new StringBuilder();
+        
+        int row = 0, column = 0;
+        int columnCounter = 0;
+        while (column < columns) {
+            char character = matrix[row][column];
+            if (character == '\0') break;
+            originalText.append(character);
+            row++;
+            column++;
+            if (row == rows) {
+                row = 0;
+                columnCounter++;
+                column = columnCounter;
+            };
 
-        for (int j = 0; j < c; j++) {
-            int i = 0, x = j;
-            while (i < r && x < c) {
-                sb.append(m[i][x]);
-                i++;
-                x++;
+        }
+        int limit = originalText.length();
+        for (int i = originalText.length() - 1; i >= 0; i--) {
+            if (originalText.charAt(i) == ' ') {
+                limit--;
+            } else {
+                break;
             }
         }
-
-        int e = sb.length() - 1;
-        while (e >= 0 && sb.charAt(e) == ' ') e--;
-
-        return sb.substring(0, e + 1);
+        originalText.setLength(limit);
+        return originalText.toString();
     }
 }
