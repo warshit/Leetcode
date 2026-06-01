c844d1d1b3c3d72e3c6a3e4eb8f0857bbf0b582e
warshit <155526453+warshit@users.noreply.github.com>
2026-06-01 17:32:25 +0530

    Time: 6 ms (99.92%), Space: 51.4 MB (72.43%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,10 +1,33 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
-        Arrays.sort(s);
-        int i=0,j=0;
-        while(i<g.length && j<s.length){ if(g[i]<=s[j]) i++;
-        j++;}
-        return i;
+        // Arrays.sort(g);
+        // Arrays.sort(s);
+        // int i=0,j=0;
+        // while(i<g.length && j<s.length){ if(g[i]<=s[j]) i++;
+        // j++;}
+        // return i;
+        if(s.length == 0)return 0;
+        Thread t1 = new Thread(() -> {Arrays.sort(g);});
+        Thread t2 = new Thread(() -> {Arrays.sort(s);});
+        t1.start();
+        t2.start();
+        try {
+        t1.join();
+        t2.join();
+        }
+        catch(InterruptedException e) {
+        }
+        int res = 0;
+        int j = g.length - 1;
+        int i = s.length - 1;
+        while(i >= 0 && j >= 0){
+            if(g[j] <= s[i]){
+                res++;
+                i--;j--;
+            }else{
+                j--;
+            }
+        }
+        return res;
     }
 }
