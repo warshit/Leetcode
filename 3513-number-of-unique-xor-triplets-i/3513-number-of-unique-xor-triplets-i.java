c1edf87bae1bccfb855c8993adb251e4e174d644
warshit <155526453+warshit@users.noreply.github.com>
2026-07-23 21:15:42 +0530

    Time: 0 ms (100%), Space: 125.6 MB (10.2%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,11 +1,15 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-         int n = nums.length;
+        //  int n = nums.length;
 
-        if (n < 3) {
-            return n;
-        }
+        // if (n < 3) {
+        //     return n;
+        // }
 
-        return Integer.highestOneBit(n) << 1;
+        // return Integer.highestOneBit(n) << 1;
+        int n = nums.length;
+        if(n < 3) return n;
+        int nearest = (int)(Math.log(n)/Math.log(2));
+        return 1 << (nearest + 1);
     }
 }
