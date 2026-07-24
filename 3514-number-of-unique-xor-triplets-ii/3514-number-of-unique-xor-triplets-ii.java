8ac50a1fb1f94c85907efc202eab97326b029f40
warshit <155526453+warshit@users.noreply.github.com>
2026-07-24 22:41:44 +0530

    Time: 259 ms (52.73%), Space: 47.1 MB (49.09%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int uniqueXorTriplets(int[] nums) {
+    public int uniqueXorTriplets(int[] arr) {
         // int n = nums.length;
 
         // Set<Integer> pairXorSet = new HashSet<>();
@@ -17,33 +17,64 @@ class Solution {
         // }
 
         // return result.size();
-           boolean[] present = new boolean[2048];
-        List<Integer> values = new ArrayList<>();
+        //    boolean[] present = new boolean[2048];
+        // List<Integer> values = new ArrayList<>();
 
-        for (int x : nums) {
-            if (!present[x]) {
-                present[x] = true;
-                values.add(x);
+        // for (int x : nums) {
+        //     if (!present[x]) {
+        //         present[x] = true;
+        //         values.add(x);
+        //     }
+        // }
+
+        // boolean[][] dp = new boolean[4][2048];
+        // dp[0][0] = true;
+
+        // for (int pick = 0; pick < 3; pick++) {
+        //     for (int xor = 0; xor < 2048; xor++) {
+        //         if (!dp[pick][xor]) continue;
+
+        //         for (int v : values) {
+        //             dp[pick + 1][xor ^ v] = true;
+        //         }
+        //     }
+        // }
+
+        // int ans = 0;
+        // for (boolean ok : dp[3]) {
+        //     if (ok) ans++;
+        // }
+        // return ans;
+
+        int n = arr.length;
+        boolean[] freq = new boolean[2048];
+        int len = 0, idx = 0, ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (!freq[arr[i] ^ arr[j]])
+                    len++;
+                freq[arr[i] ^ arr[j]] = true;
             }
         }
 
-        boolean[][] dp = new boolean[4][2048];
-        dp[0][0] = true;
+        int[] nums = new int[len];
 
-        for (int pick = 0; pick < 3; pick++) {
-            for (int xor = 0; xor < 2048; xor++) {
-                if (!dp[pick][xor]) continue;
+        for (int i = 0; i < 2048; i++)
+            if (freq[i])
+                nums[idx++] = i;
 
-                for (int v : values) {
-                    dp[pick + 1][xor ^ v] = true;
-                }
-            }
+        Arrays.fill(freq, false);
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < n; j++)
+                freq[nums[i] ^ arr[j]] = true;
         }
 
-        int ans = 0;
-        for (boolean ok : dp[3]) {
-            if (ok) ans++;
-        }
+        for (boolean b : freq)
+            if (b)
+                ans++;
+
         return ans;
     }
 }
